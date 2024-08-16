aa70e1ddc4fbefb0a395689998f205a3d4e07f7c
Javokhir <javokhir.akramjonov@gmail.com>
2024-08-17 01:13:26 +0200

    feat: Implement exams bloc and events

---
+++ lib/ui/screen/exams/bloc/exams_event.dart
@@ -0,0 +1,6 @@
+part of 'exams_bloc.dart';
+
+@freezed
+class ExamsEvent with _$ExamsEvent {
+  const factory ExamsEvent.started() = _Started;
+}
---
+++ lib/ui/screen/exams/bloc/exams_state.dart
@@ -0,0 +1,6 @@
+part of 'exams_bloc.dart';
+
+@freezed
+class ExamsState with _$ExamsState {
+  const factory ExamsState.initial() = _Initial;
+}
