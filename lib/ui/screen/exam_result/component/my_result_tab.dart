000f8e4b93c0f04b1a6b1eff8190adfa0e45158e
Javokhir <javokhir.akramjonov@gmail.com>
2024-10-03 23:04:02 +0200

    make my result tab scrollable

--- lib/ui/screen/exam_result/component/my_result_tab.dart
+++ lib/ui/screen/exam_result/component/my_result_tab.dart
@@ -11,182 +11,186 @@ class MyResultTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(alignment: Alignment.topCenter, children: [
-      Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          children: [
-            Container(
-              padding: const EdgeInsets.all(48),
-              decoration: const BoxDecoration(
-                color: Color(0xFF0D0D0D),
-                borderRadius: BorderRadius.all(Radius.circular(16)),
-                border: Border.fromBorderSide(BorderSide(
-                  color: Color(0xFF1C1C1C),
-                )),
-              ),
-              alignment: Alignment.center,
-              child: Column(
-                children: [
-                  SvgPicture.asset(
-                    'assets/icons/cup_dark.svg',
-                    width: 80,
-                  ),
-                  const SizedBox(height: 34),
-                  Text.rich(
-                    TextSpan(
-                      children: [
-                        const TextSpan(
-                          text: 'You got',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Color(0xFFC5CCDB),
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        TextSpan(
-                          text: ' +${examResult.score} ',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Theme.of(context).colorScheme.primary,
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ),
-                        const TextSpan(
-                          text: 'points',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Color(0xFFC5CCDB),
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 18),
-            Row(
+    return Expanded(
+      child: Stack(alignment: Alignment.topCenter, children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: SingleChildScrollView(
+            child: Column(
               children: [
-                Expanded(
-                  child: Container(
-                    padding: const EdgeInsets.all(16),
-                    decoration: BoxDecoration(
-                      color: const Color(0xFF0D0D0D),
-                      borderRadius: BorderRadius.circular(16),
-                      border: Border.all(
-                        color: Theme.of(context).colorScheme.primary,
+                Container(
+                  padding: const EdgeInsets.all(48),
+                  decoration: const BoxDecoration(
+                    color: Color(0xFF0D0D0D),
+                    borderRadius: BorderRadius.all(Radius.circular(16)),
+                    border: Border.fromBorderSide(BorderSide(
+                      color: Color(0xFF1C1C1C),
+                    )),
+                  ),
+                  alignment: Alignment.center,
+                  child: Column(
+                    children: [
+                      SvgPicture.asset(
+                        'assets/icons/cup_dark.svg',
+                        width: 80,
                       ),
-                    ),
-                    child: Column(
-                      children: [
-                        Text(
-                          examResult.correctCount.toString(),
-                          style: TextStyle(
+                      const SizedBox(height: 34),
+                      Text.rich(
+                        TextSpan(
+                          children: [
+                            const TextSpan(
+                              text: 'You got',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Color(0xFFC5CCDB),
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            TextSpan(
+                              text: ' +${examResult.score} ',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Theme.of(context).colorScheme.primary,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                            const TextSpan(
+                              text: 'points',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Color(0xFFC5CCDB),
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 18),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        padding: const EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                          color: const Color(0xFF0D0D0D),
+                          borderRadius: BorderRadius.circular(16),
+                          border: Border.all(
                             color: Theme.of(context).colorScheme.primary,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w700,
                           ),
                         ),
-                        const Text(
-                          'Correct Answers',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                          ),
+                        child: Column(
+                          children: [
+                            Text(
+                              examResult.correctCount.toString(),
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.primary,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                            const Text(
+                              'Correct Answers',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 8),
-                Expanded(
-                  child: Container(
-                    padding: const EdgeInsets.all(16),
-                    decoration: BoxDecoration(
-                      color: const Color(0xFF0D0D0D),
-                      borderRadius: BorderRadius.circular(16),
-                      border: Border.all(
-                        color: Theme.of(context).colorScheme.error,
                       ),
                     ),
-                    child: Column(
-                      children: [
-                        Text(
-                          examResult.inCorrectCount.toString(),
-                          style: TextStyle(
+                    const SizedBox(width: 8),
+                    Expanded(
+                      child: Container(
+                        padding: const EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                          color: const Color(0xFF0D0D0D),
+                          borderRadius: BorderRadius.circular(16),
+                          border: Border.all(
                             color: Theme.of(context).colorScheme.error,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w700,
                           ),
                         ),
-                        const Text(
-                          'Incorrect Answers',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                          ),
+                        child: Column(
+                          children: [
+                            Text(
+                              examResult.inCorrectCount.toString(),
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.error,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                            const Text(
+                              'Incorrect Answers',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF0D0D0D),
+                    borderRadius: BorderRadius.circular(16),
+                    border: Border.all(
+                      color: const Color(0xFF1C1C1C),
                     ),
                   ),
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    children: [
+                      Text(
+                        examResult.duration
+                            .substring(0, examResult.duration.indexOf('.')),
+                        style: const TextStyle(
+                          color: Color(0xFFC5CCDB),
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        'Spent time',
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
+                const SizedBox(height: 8),
+                _openExamReviewButton(context),
               ],
-            ),
-            const SizedBox(height: 8),
-            Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: const Color(0xFF0D0D0D),
-                borderRadius: BorderRadius.circular(16),
-                border: Border.all(
-                  color: const Color(0xFF1C1C1C),
-                ),
-              ),
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                children: [
-                  Text(
-                    examResult.duration
-                        .substring(0, examResult.duration.indexOf('.')),
-                    style: const TextStyle(
-                      color: Color(0xFFC5CCDB),
-                      fontSize: 20,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    'Spent time',
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.primary,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 8),
-            _openExamReviewButton(context),
-          ],
-        ),
-      ),
-      if (examResult.score > 0)
-        IgnorePointer(
-          child: Transform.scale(
-            scale: 2,
-            child: LottieBuilder.asset(
-              height: 400,
-              'assets/animations/congrats.json',
-              repeat: false,
             ),
           ),
         ),
-    ]);
+        if (examResult.score > 0)
+          IgnorePointer(
+            child: Transform.scale(
+              scale: 2,
+              child: LottieBuilder.asset(
+                height: 400,
+                'assets/animations/congrats.json',
+                repeat: false,
+              ),
+            ),
+          ),
+      ]),
+    );
   }
 
   Widget _openExamReviewButton(BuildContext context) {
